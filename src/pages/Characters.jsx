a4aa31e9c0c1744addea1a2fcd8a1ef327708705
Dong-Yon Kim <dongyonkim5@gmail.com>
2024-04-16 02:37:06 -0500

    updating files, adding in missing content

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,1 +1,9 @@
 
+export default function Characters() {
+  // goals: implement an api on a website, probably github
+  return (
+    <div>
+      <p>Placeholder text for characters</p>
+    </div>
+  );
+}
