082a0a5e40b9e83527b07d55b7d1119323bea486
Joyce12345678 <73724033+Joyce12345678@users.noreply.github.com>
2024-04-16 04:07:06 -0400

    Rules.jsx

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -1,26 +1,31 @@
 export default function Rules(){
     return(
         <div>
-            <h1>Genshin Ranked rulebook</h1>
+            <h1>Genshin Ranked Rulebook</h1>
             <p style="font-size:20px">GENSHIN RANKED is a CO-OP 3v3 gamemode where players are organized in teams of 3 to fight a series of bosses. Whichever team can kill the bosses faster will win.</p>
-            <p style="font-size:18px">DIVISIONS:<p>
-            </p>Currently, there are THREE different divisions: OPEN, ADVANCED, and PREMIER. 									
+            
+            <p style="font-size:18px"><b>DIVISIONS:</b><br>
+            <font size="2.9">Currently, there are THREE different divisions: OPEN, ADVANCED, and PREMIER. 									
 		These divisions are separated based off of INVESTMENT, and units in the same division should perform similarly. During any division match, all players may only use characters and weapons within that division OR a lower division.									
 		Refer to the characters section on this website to determine your division.						
-                If you need assistance, please join the DISCORD SERVER (discord.gg/fnGdP36E2Q) and ask a member to help!<p>
-            <p style="font-size:18px">GAMEMODES <p>
-            </p>RANKED has 2 different gamemodes: DRAFT PICK and BLIND PICK								
+                If you need assistance, please join the DISCORD SERVER (discord.gg/fnGdP36E2Q) and ask a member to help!</font></p>
+            
+            <p style="font-size:18px"><b>GAMEMODES:</b><br>
+            <font size="2.9">RANKED has 2 different gamemodes: DRAFT PICK and BLIND PICK								
 		DRAFT PICK involves banning different characters for BOTH teams. Additionally, the character picks across teams may NOT overlap.								
 		BLIND PICK involves each team picking a set of characters to use, with no bans. Character picks across teams may overlap.								
 		It is generally recommended for newer players to start with BLIND PICK, but the game may be played however you wish. 								
-		HAVE FUN!<p>
-		  <p style="font-size:18px">GAMEPLAY 3V3 RULES<p>
-		  <p>WL8, 1 ref per world, no consumables, no gadgets, no external assistance. <p>
-		  <p style="font-size:16px"> BOSS PICKS<br>
+		HAVE FUN!</font></p>
+		  
+		  <p style="font-size:18px"><b>GAMEPLAY 3V3 RULES</b><br>
+		  <font size="3">WL8, 1 ref per world, no consumables, no gadgets, no external assistance.</font></p>
+		  
+		  <p style="font-size:16px"><u>BOSS PICKS</u><br>
 		2x alternate team 1 picking one, team 2 picking the other. 
 		Drake is the first boss.
-		Matches will be determined by a best 7, or 9, depending on the division. <br>
-		<p style="font-size:16px">CHARACTER PICKS</br> 
+		Matches will be determined by a best 7, or 9, depending on the division.<br>
+		
+		<p style="font-size:16px"><u>CHARACTER PICKS</u></br> 
 		A ban completely removes the character from both teams' pools. currently, do not allow duplicate characters.
 		You have 30 seconds to decide on a ban, else, it is forfeit. You have 30 seconds to decide on a character. You may hover a selection by typing "hover" before a pick.
 		depending on draft or blind, the pick order may be different. 
@@ -34,12 +39,14 @@ export default function Rules(){
 		kill the thing as fast as you can
 		ref will stop timer
 		in cases of a solo ref, team 1 goes first, then team 2
-		<br><p style="font-size:16px">ACCIDENTS</br> 
+		
+		<br><p style="font-size:16px"><u>ACCIDENTS</u></br> 
 		Do not respawn until the boss is complete
 		do not heal to full or obtain energy
 		if both teams die, the team that survived for the longest amount of time wins
-		only legal heals occur while the match timer is active
-		<br><p style="font-size:16px">FREE RESET</br> 
+		only legal heals occur while the match timer is active<br>
+		
+		<p style="font-size:16px"><u>FREE RESET</u></br> 
 		every team is allowed one free reset to account for technical issues .
 		2 min roll call for HP and energy as desired</p>
         </div>
