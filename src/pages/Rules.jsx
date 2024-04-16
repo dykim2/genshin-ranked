2fa39b31d05c0c8944d81efa579f4bc03175ad99
Joyce12345678 <73724033+Joyce12345678@users.noreply.github.com>
2024-04-16 02:59:55 -0400

    Rules.jsx

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -4,17 +4,17 @@ export default function Rules(){
             <h1>Genshin Ranked rulebook</h1>
             <p>GENSHIN RANKED is a CO-OP 3v3 gamemode where players are organized in teams of 3 to fight a series of bosses. Whichever team can kill the bosses faster will win.</p>
             <p>DIVISIONS:								
-			    Currently, there are THREE different divisions: OPEN, ADVANCED, and PREMIER. 									
-				These divisions are separated based off of INVESTMENT, and units in the same division should perform similarly. During any division match, all players may only use characters and weapons within that division OR a lower division.									
-				Refer to the characters to determine your division.						
+		Currently, there are THREE different divisions: OPEN, ADVANCED, and PREMIER. 									
+		These divisions are separated based off of INVESTMENT, and units in the same division should perform similarly. During any division match, all players may only use characters and weapons within that division OR a lower division.									
+		Refer to the characters to determine your division.						
                 If you need assistance, please join the DISCORD SERVER and ask a member to help!									
                 The link to join our discord is at the TOP of this sheet.<p>
             <p>GAMEMODES								
-			    RANKED has 2 different gamemodes: DRAFT PICK and BLIND PICK								
-				DRAFT PICK involves banning different characters for BOTH teams. Additionally, the character picks across teams may NOT overlap.								
-				BLIND PICK involves each team picking a set of characters to use, with no bans. Character picks across teams may overlap.								
-				It is generally recommended for newer players to start with BLIND PICK, but the game may be played however you wish. 								
-				HAVE FUN!<p>
+		RANKED has 2 different gamemodes: DRAFT PICK and BLIND PICK								
+		DRAFT PICK involves banning different characters for BOTH teams. Additionally, the character picks across teams may NOT overlap.								
+		BLIND PICK involves each team picking a set of characters to use, with no bans. Character picks across teams may overlap.								
+		It is generally recommended for newer players to start with BLIND PICK, but the game may be played however you wish. 								
+		HAVE FUN!<p>
         </div>
     )
 }
