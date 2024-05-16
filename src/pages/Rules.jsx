9cd0762a146fb69c0b9fd0f0775a644044057fcc
Joyce12345678 <73724033+Joyce12345678@users.noreply.github.com>
2024-05-16 09:58:28 -0400

    Rules.jsx

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -8,7 +8,7 @@ export default function Rules(){
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <style>
             div {
-              margin: 25px 75px 50px 75px;
+              margin: 25px 96px 50px 96px;
             }
             body {
               background-color: black;
