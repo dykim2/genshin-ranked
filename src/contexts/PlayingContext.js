c3fae4047892c9e58ceb3f57eb9705b641d64fcd
chymchym1905 <kien.dauoofman@hcmut.edu.vn>
2024-06-06 03:55:17 +0700

    fix websocket url

--- src/contexts/PlayingContext.js
+++ src/contexts/PlayingContext.js
@@ -1,9 +1,9 @@
 import { createContext } from "react";
 
 export const PlayingContext = createContext();
-export let socket = new WebSocket("https://rankedwebsocketapi.fly.dev/");
+export let socket = new WebSocket(`wss://rankedwebsocketapi.fly.dev/`);
 export function restartSocket(){
     if(socket.readyState == 3){
-        socket = new WebSocket("https://rankedwebsocketapi.fly.dev/");
+        socket = new WebSocket(`wss://rankedwebsocketapi.fly.dev/`);
     }
 }
