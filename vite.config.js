885974d8614a7ab4d0ca5eb7396775d73dc5ea87
dykim2 <68031913+dykim2@users.noreply.github.com>
2024-06-16 22:13:05 -0500

    more link fixes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/genshin-ranked/",
   build: {
     sourcemap: true,
-    base: "/genshin-ranked/",
   },
 });
