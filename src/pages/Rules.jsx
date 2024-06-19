a8d881b35e019ba8aa3d32baca87704714477bc6
dykim2 <68031913+dykim2@users.noreply.github.com>
2024-06-18 20:14:34 -0500

    update rules info

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -7,60 +7,74 @@ export default function Rules() {
       <div style={{ color: "white", marginLeft: 20 }}>
         <h1>GENSHIN RANKED 3v3</h1>
         <br />
-GAME FORMAT
+<h2>GAME FORMAT</h2>
 <br />
-Draft Phase: Prep
+<h3>Draft Phase: Prep</h3>
+<br />
 
 If you are queuing an unscheduled match, 5 minutes of prep time will be given to both teams in order to discuss strategies and share rosters.
 <br />
-
-Draft Phase: Bosses
+<br />
+<h3> Draft Phase: Bosses </h3>
 <br />
 Teams will alternate between picking bosses, starting with Team 1. For Open and Advanced, each team picks 3 bosses. For Premier, each team picks 4 bosses.
 <br />
 Aeonblight Drake is the first boss.
 <br />
 Teams may not pick more than ONE premium boss. These include: Oceanid, all weekly bosses, and all local legends.
+<br />
 
 Weekly bosses cannot be picked in Open and local legends cannot be picked in Open or Advanced.
+<br />
 
 <br />
-<h2>Draft Phase: Characters</h2>
+<h3>Draft Phase: Characters</h3>
 <br />
 
 NOTE: The information below applies only to 3v3 draft. In blind, teams are given 3 minutes to freely choose their 6 characters. Duplicates are allowed between the teams (ex. both teams can choose Yelan) but not within one team (ex. one team cannot play two Yelans).
+<br />
 
 Banning a character removes the character from both teams' pools. Picking a character removes the character from the other team, and picking duplicates is not allowed.
+<br />
 
 Each character pick/ban is 30 seconds. If a ban is not made before 30 seconds, it is forfeit. 
+<br />
 
 You may hover a selection by typing "hover" before a pick.
+<br />
 
 Refer to the scoreboard for drafting boards and pick order.
 <br />
+<br />
  
-Gameplay: Prep
-
+<h3>Gameplay: Prep</h3>
+<br />
 Bosses may not be killed in the ref world outside of designated prep time, including prep before draft, drafting, and roll call. If this rule is violated, prep time will be reduced.
-
+<br />
 The amount of time allocated for boss prep is determined by division. Teams may do anything in the ref world during this time. The ref will start any domains/bosses that need host interaction.
-
+<br />
 After this prep time, there is a 2 minute roll call. Refs will invite players to a domain to get energy, if necessary, and teams can set their HP and energy as desired.
-
+<br />
 During the roll call, teams will tell the ref which two characters out of the six picks each player is going to use. Additionally, players will have to put both characters into their showcase, with show character details enabled.
-
-
-Gameplay: Bosses
+<br />
+<br />
+<h3>Gameplay: Bosses</h3>
+<br />
 
 All bosses will be done in WL8. Gadgets, consumables, and external assistance are not allowed.
+<br />
 
 When you are ready to start a boss, please type "r" or "ready" in the chat.
+<br />
 
 Wait until the ref confirms the timer is ready before starting the boss. In the case of tech issues, players are free to let the ref know. Whether a retry is offered or not is up to the ref's discretion.
+<br />
 
 If any player dies during the boss, do not respawn until the boss is complete, heal, or obtain energy. If both teams are teamwiped, the team who survived the longest will win the boss.
+<br />
 
 Pre-casting is allowed, but players may not cast a healing ability before the boss is activated.
+<br />
 
 Every team is allowed one free reset after all bosses have been completed. There is a 2 min roll call to obtain desired HP and energy. 
       </div>
