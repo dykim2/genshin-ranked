109634db6b68d888707ae15d23fad304d3b407a1
kura <86320194+kurazl@users.noreply.github.com>
2024-07-13 23:12:05 -0700

    Add Roboto-Mono

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,12 @@ import { AppLayoutWrapper } from "./components";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./utils";
 
+// https://mui.com/material-ui/getting-started/installation/#roboto-font
+import "@fontsource/roboto-mono/300.css";
+import "@fontsource/roboto-mono/400.css";
+import "@fontsource/roboto-mono/500.css";
+import "@fontsource/roboto-mono/700.css";
+
 function App() {
 	return (
 		<BrowserRouter basename="/">
