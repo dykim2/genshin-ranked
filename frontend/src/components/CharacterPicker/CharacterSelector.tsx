a08b29f050cdd3784e49fc324e9ec4e8eb94b4e4
kura <86320194+kurazl@users.noreply.github.com>
2024-07-14 05:56:46 -0700

    Search Menu For Character Picker

--- frontend/src/components/CharacterPicker/CharacterSelector.tsx
+++ frontend/src/components/CharacterPicker/CharacterSelector.tsx
@@ -6,38 +6,56 @@ import {
 	// RARITY,
 	// RARITY_INFO,
 } from "@genshin-ranked/shared";
-import { Grid, Stack } from "@mui/material";
+import { Grid, Stack, TextField } from "@mui/material";
 import { CharacterButton } from "./CharacterButton";
 import { GroupToggle } from "../GroupToggle";
 import { CHARACTER_INFO } from "@genshin-ranked/shared/src/types/characters/details";
 
 export const CharacterSelector = () => {
 	const [elementFilter, setElementFilter] = useState<ELEMENTS | null>(null);
+	const [searchFilter, setSearchFilter] = useState<string>("");
+
+	console.log(searchFilter);
 
 	return (
 		<Stack direction="column">
-			<Stack direction="row">
+			<Stack direction="row" alignContent="center">
+				<TextField
+					placeholder="Search"
+					value={searchFilter}
+					onChange={(e) => {
+						setSearchFilter(e.target.value);
+					}}
+				/>
 				<GroupToggle
 					value={elementFilter}
 					setValue={setElementFilter}
 					options={Object.values(ELEMENT_INFO)}
 				/>
-				{/* <GroupToggle
-					value={rarityFilter}
-					setValue={() => setRarityFilter}
-					options={Object.values(RARITY_INFO)}
-				/> */}
 			</Stack>
 			<Grid container spacing={1} justifyContent="center">
+				{/* TODO: Wrap this with useMemo to minimize unnessecary refiltering of these values */}
 				{Object.values(CHARACTERS)
 					.filter((char) => {
-						if (!elementFilter) return true;
-						console.log(
-							elementFilter,
-							CHARACTER_INFO[char].element,
-						);
+						if (searchFilter.length === 0 && elementFilter === null)
+							return true;
 
-						return elementFilter === CHARACTER_INFO[char].element;
+						// TODO: Algorithm can be slightly optimized. (Not by degrees of n, but perhaps a coefficient of it.)
+						const correctElement =
+							elementFilter === CHARACTER_INFO[char].element;
+						const correctSearch = CHARACTER_INFO[char].imageFileName
+							.toLowerCase()
+							.includes(searchFilter.toLowerCase());
+
+						if (searchFilter.length === 0 && elementFilter) {
+							return correctElement;
+						} else if (searchFilter.length > 0 && !elementFilter) {
+							return correctSearch;
+						} else if (searchFilter.length > 0 && elementFilter) {
+							return correctElement && correctSearch;
+						} else {
+							return true;
+						}
 					})
 					.map((x) => (
 						<Grid item padding={1} key={x}>
