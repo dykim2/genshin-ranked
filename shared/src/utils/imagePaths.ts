72d239bdef37587b5e3059bb56cc9b39ced2f7b1
kura <86320194+kurazl@users.noreply.github.com>
2024-07-13 21:33:32 -0700

    getCharacterImagePath Util Function

--- shared/src/utils/imagePaths.ts
+++ shared/src/utils/imagePaths.ts
@@ -1,3 +1,10 @@
-export const ELEMENT_IMAGE_PATH = "/images/general/elements"
-export const WEAPON_IMAGE_PATH = "/image/general/billets"
-export const CHARACTER_IMAGE_PATH = "/image/chars"
+import { CHARACTER_NAME } from "../types";
+import { CHARACTERS } from "../types/characters/details";
+
+export const ELEMENT_IMAGE_PATH = "/images/general/elements";
+export const WEAPON_IMAGE_PATH = "/image/general/billets";
+export const CHARACTER_IMAGE_PATH = "/image/chars";
+
+export const getCharacterImagePath = (character: CHARACTER_NAME) => {
+	return `${CHARACTER_IMAGE_PATH}/${CHARACTERS[character].imageFileName}.png`;
+};
