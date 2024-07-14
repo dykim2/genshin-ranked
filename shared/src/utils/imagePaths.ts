e612fc87484be4052e54dbeac3eec910515a91fe
kura <86320194+kurazl@users.noreply.github.com>
2024-07-13 21:46:30 -0700

    Fix Image Path Bug + Element Path Added

--- shared/src/utils/imagePaths.ts
+++ shared/src/utils/imagePaths.ts
@@ -2,9 +2,13 @@ import { CHARACTER_NAME } from "../types";
 import { CHARACTERS } from "../types/characters/details";
 
 export const ELEMENT_IMAGE_PATH = "/images/general/elements";
-export const WEAPON_IMAGE_PATH = "/image/general/billets";
-export const CHARACTER_IMAGE_PATH = "/image/chars";
+export const WEAPON_IMAGE_PATH = "/images/general/billets";
+export const CHARACTER_IMAGE_PATH = "/images/chars";
 
 export const getCharacterImagePath = (character: CHARACTER_NAME) => {
 	return `${CHARACTER_IMAGE_PATH}/${CHARACTERS[character].imageFileName}.png`;
+};
+
+export const getCharacterElementImagePath = (character: CHARACTER_NAME) => {
+	return `${ELEMENT_IMAGE_PATH}/${CHARACTERS[character].element}.png`;
 };
