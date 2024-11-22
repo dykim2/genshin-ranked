3595a206d43c0b097b238d540a5cd743e3336fb4
SodiqBalogun <sbalogun157@gmail.com>
2024-11-22 17:42:53 -0600

    Changed the "OneCharacter" to a function that is then returned. Also destructured props. Also added the restrictions of all characters in "testingCharRestrictions.tsx"

--- src/pages/OneCharacter.jsx
+++ src/pages/OneCharacter.jsx
@@ -1,18 +1,32 @@
-
 import { Link } from "react-router-dom";
 
-export default function OneCharacter(props){
-    return (
-      <div>
-        <Link style={{ color: "white" }} to="/characters">
-          {"< Return to characters"}
-        </Link>
-        <p>{props.name}</p>
-        <img src={props.img} height={1500} />
-      </div>
-    );
+// Destructuring the props (Idk how many props are being used :c)
+const OneCharacter = ({name, img} = props) => {
+  return (
+    <div>
+      <Link style={{ color: "white" }} to="/characters">
+        {"< Return to characters"}
+      </Link>
+      <p>{name}</p>
+      <img src={img} height={1500} />
+      
+    </div>
+  )
 }
 
+// export default function OneCharacter(props){
+//     return (
+//       <div>
+//         <Link style={{ color: "white" }} to="/characters">
+//           {"< Return to characters"}
+//         </Link>
+//         <p>{props.name}</p>
+//         <img src={props.img} height={1500} />
+//       </div>
+//     );
+// }
+
+export default OneCharacter;
 
 /*
 <img
